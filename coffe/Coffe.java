c6a9ec0d2bd908a9ba8c06699ceba925010cd262
jusufsemsovic <94207219+jukajuka123@users.noreply.github.com>
2022-10-26 20:36:42 +0200

    Add files via upload

---
+++ coffe/Coffe.java
@@ -0,0 +1,16 @@
+package coffe;
+
+public class Coffe {
+	
+	public boolean Empty() {
+		boolean empty = false;
+		return empty;
+	}
+	void Reffil(){
+		System.out.println("Coffe reffiled");
+	}
+	void Drink() {
+		System.out.println("You are drinking coffe!");
+	}
+}
+
